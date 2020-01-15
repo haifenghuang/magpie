1e0f2f2e16fd610866984f94143871165dc75e30
haifenghuang <fenghai_hhf@163.com>
2020-01-15 22:46:38 +0800

    Service shutdown improvment: shutdown gracefully

--- src/magpie/eval/service.go
+++ src/magpie/eval/service.go
@@ -8,13 +8,17 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"magpie/ast"
 	"net/http"
 	"net/url"
+	"os"
+	"os/signal"
 	"path"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -68,7 +72,45 @@ func (s *ServiceObj) Run(line string, args ...Object) Object {
 	if args[0].(*Boolean).Bool {
 		s.Router.Use(LoggingMiddleware)
 	}
-	http.ListenAndServe(s.Addr, s.Router)
+
+	srv := &http.Server{
+		Addr: s.Addr,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler: s.Router,
+    }
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	wait := time.Second * 15
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	//log.Println("shutting down...")
+
+	close(c)
+	os.Exit(0)
 
 	return NIL
 }
